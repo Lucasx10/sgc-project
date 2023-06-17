2fb3f42127641ca43e35a64d49a32d3f8d1a1ebe
Lucas Prado <lukasprado260@gmail.com>
2023-06-17 13:11:58 -0400

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -5,12 +5,24 @@ const user = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
       },
+      endereco: {
+        type: DataTypes.STRING,
+      },
+      image: {
+        type: DataTypes.STRING,
+      },
       email: {
         type: DataTypes.STRING,
       },
       password: {
         type: DataTypes.STRING,
       },
+      whatsapp: {
+        type: DataTypes.STRING,
+      },
+      isAtivo: {
+        type: DataTypes.BOOLEAN,
+      }
     },
     {
       tableName: "user",
